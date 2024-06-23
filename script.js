99a5b56c5edb26f0f1fc14dfa71bce66f7d93302
Yassin Ibrahim <omarothman@Mays-MacBook-Pro.local>
2024-06-23 21:51:56 +0400

    mid commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear')
+const itemFilter = document.getElementById('filter');
 
 function addItem(e) {
     e.preventDefault();
@@ -20,6 +22,8 @@ function addItem(e) {
 
     itemList.appendChild(li);
 
+    checkUI();
+
     itemInput.value = '';
 }
 
@@ -37,4 +41,55 @@ function createIcon(classes) {
     return icon;
 }
 
-itemForm.addEventListener('submit', addItem); 
+
+function removeItem(e) {
+     if (e.target.parentElement.classList.contains('remove-item')) {
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+
+            checkUI();
+        }
+     }
+}
+
+function clearItems() {
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+    }
+    
+    checkUI();
+}
+
+function filterItems(e) {
+    const items = itemList.querySelectorAll('li');
+    const text = e.target.value.toLowerCase();
+
+    items.forEach((item) => {
+        const itemName = item.firstChild.textContent.toLowerCase();
+
+        if (itemName.indexOf(text) != -1) {
+            item.style.display = 'flex';
+        }
+        else {
+            item.style.display = 'none';
+        }
+    });
+}
+
+function checkUI() {
+    const items = itemList.querySelectorAll('li');
+    if (items.length === 0) {
+        clearBtn.style.display = 'none';
+        itemFilter.style.display = 'none';
+    } else {
+        clearBtn.style.display = 'block';
+        itemFilter.style.display = 'block'; 
+    }
+}
+
+itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
+
+checkUI();
